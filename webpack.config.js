4c3cf48fd23e8539e344e2572727693de2a27655
CortesCode <cacortesl01@gmail.com>
2023-01-01 18:06:16 -0500

    Se crea proyecto con npm, se agregan dependencias y se comienza la estructura del documento html

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {//Identifica las diferentes extesiones que tiene el proyecto, osea los .algo
+        extensions:[".js"]
+    },
+    module: {
+        rules: [ 
+            {
+                test: /\.js?$/, //utilizar extensiones de archivo .js
+                exclude: /node_modules/, //excluir esta carpeta
+                use: {
+                    loader: 'babel-loader',//uso del loader
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html',
+        })
+    ]
+}
