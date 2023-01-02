7a1b2cc6ffb71c199a4c273e4360dee9d179cad3
CortesCode <cacortesl01@gmail.com>
2023-01-02 15:40:05 -0500

    Se crean los scripts para hacer build y start del server local. Se corrige fuente de la imagen de perfil y se añade la dependecia de gh-pages.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
